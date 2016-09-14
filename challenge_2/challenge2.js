629cb4a7ee675d5e323542149207931b762898bf
Jamal <jamal.l.mcdowell@gmail.com>
2016-09-14 12:49:07 -0400

    completed firstChallenge2

--- challenge_2/challenge2.js
+++ challenge_2/challenge2.js
@@ -1,5 +1,22 @@
-$(function(){
+$(function() {
+    var colorSwitch = true;
 
-	//all your code should go here
+    $("h1").click(function() {
+        if (colorSwitch)
+            $("h1").css('color', 'red');
+        else {
+            $("h1").css('color', 'black');
+        }
+        colorSwitch = !colorSwitch;
+    });
+
+    $("h5:odd").hide();
+
+    $("p").not("aside p").text().replace("Bacon", "LASER VISION");
+
+    $(".post:last-of-type").remove();
+    $(".post:last-of-type").remove();
+
+    $("aside img").remove();
 
 })
